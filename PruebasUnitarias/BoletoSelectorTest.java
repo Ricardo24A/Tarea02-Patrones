095c3ccf13660e0962c7337a7967fd81b4b50af5
Jose Andres Adrian Fierro <jadrian@espol.edu.ec>
2025-01-30 21:50:16 -0500

    PruebaUnitaria BoletoSelectorTest

--- PruebasUnitarias/BoletoSelectorTest.java
+++ PruebasUnitarias/BoletoSelectorTest.java
@@ -1,13 +1,14 @@
-package PruebasUnitarias;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ec.edu.espol.ticketbeat.PatronFactoryMethod;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import PatronFactoryMethod.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -51,4 +52,3 @@ class BoletoSelectorTest {
         assertNotNull(selectorReservado.crearBoleto());
     }
 }
-
